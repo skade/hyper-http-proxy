769a704256a0ae186414d0e03a902605d5512bb6
Florian Gilcher <florian.gilcher@asquera.de>
2018-08-09 11:46:59 +0200

    Port to hyper 0.12

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,86 @@
 extern crate futures;
 extern crate hyper;
-extern crate tokio_core;
 
-use futures::future::Future;
-use futures::future::ok;
-use futures::Stream;
-
-use tokio_core::reactor::Core;
-use tokio_core::reactor::Handle;
-
-use hyper::{Get, Post, StatusCode};
-use hyper::header::ContentLength;
-use hyper::server::{Http, Service, Request, Response};
+use futures::future;
+use hyper::rt::{Future, Stream};
+use hyper::service::{service_fn, Service};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use hyper::client;
-use hyper::Method;
 
-static INDEX: &'static [u8] = b"Try POST /echo";
+/// We need to return different futures depending on the route matched,
+/// and we can do that with an enum, such as `futures::Either`, or with
+/// trait objects.
+///
+/// A boxed Future (trait object) is used as it is easier to understand
+/// and extend with more types. Advanced users could switch to `Either`.
+type BoxFut = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
 
-struct Proxy {
-    handle: Handle 
-}
+struct Proxy;
 
 impl Service for Proxy {
-    type Request = Request;
-    type Response = Response;
+    type ReqBody = Body;
+    type ResBody = Body;
     type Error = hyper::Error;
-    type Future = Box<Future<Item=Response, Error=hyper::Error>>;
+    type Future = BoxFut;
+    /// This is our service handler. It receives a Request, routes on its
+    /// path, and returns a Future of a Response.
+    fn call(&mut self, req: Request<Body>) -> BoxFut {
+        let (parts, body) = req.into_parts();
 
-    fn call(&self, req: Request) -> Self::Future {
-        match (req.method(), req.path()) {
-            (&Get, "/") | (&Get, "/echo") => {
-                Box::new(ok(Response::new()
-                    .with_header(ContentLength(INDEX.len() as u64))
-                    .with_body(INDEX)))
-            },
-            (&Post, "/post") => {
-                let url = format!("http://httpbin.org/post").parse().unwrap();
-                let handle = self.handle.clone();
-                let client = client::Client::new(&handle);
+        match (parts.method, parts.uri.path()) {
+            // Serve some instructions at /
+            (Method::GET, "/") => {
+                let mut response = Response::new(Body::empty());
 
-                let mut request = client::Request::new(Method::Post, url);
-                let length = req.headers().get::<ContentLength>().unwrap().clone();
+                *response.body_mut() = Body::from("Try POSTing data to /echo");
+                Box::new(future::ok(response))
+            }
 
-                request.headers_mut().set(length);
+            // Reverse the entire body before sending back to the client.
+            //
+            // Since we don't know the end yet, we can't simply stream
+            // the chunks as they arrive. So, this returns a different
+            // future, waiting on concatenating the full body, so that
+            // it can be reversed. Only then can we return a `Response`.
+            (Method::POST, _) => {
+                let url = format!("http://httpbin.org/post");
+                let client = client::Client::new();
 
-                request.set_body(req.body());
+                let mut request_builder = hyper::Request::post(url);
+                let length = parts.headers.get("Content-Length").unwrap().clone();
+
+                request_builder.header("Content-Length", length);
+
+                let request = request_builder.body(body).unwrap();
+
                 let f = client.request(request);
 
                 Box::new(f)
-            },
+            }
+
+            // The 404 Not Found route...
             _ => {
-                Box::new(ok(Response::new()
-                    .with_status(StatusCode::NotFound)))
+                let mut response = Response::new(Body::empty());
+
+                *response.status_mut() = StatusCode::NOT_FOUND;
+                Box::new(future::ok(response))
             }
         }
-    }
 
+    }
 }
 
+fn spawn_service() -> Result<Proxy, hyper::Error> {
+    Ok(Proxy)
+}
 
 fn main() {
-    let addr = "127.0.0.1:1337".parse().unwrap();
+    let addr = ([127, 0, 0, 1], 3000).into();
 
-    let mut core = Core::new().unwrap();
-    let handle = core.handle();
-    let service_handle = core.handle();
+    let server = Server::bind(&addr)
+        .serve(|| spawn_service() )
+        .map_err(|e| eprintln!("server error: {}", e));
 
-    let server = Http::new().serve_addr_handle(&addr, &handle, move || Ok(Proxy { handle: service_handle.clone() })).unwrap();
-
-    let handle1 = handle.clone();
-    handle.spawn(server.for_each(move |conn| {
-        handle1.spawn(conn.map(|_| ()).map_err(|err| println!("srv1 error: {:?}", err)));
-        Ok(())
-    }).map_err(|_| ()));
-
-    core.run(futures::future::empty::<(), ()>()).unwrap();
+    println!("Listening on http://{}", addr);
+    hyper::rt::run(server);
 }
